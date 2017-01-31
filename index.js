364f33752934dca6234a4223108203f714d72ffe
KAWABATA Norio <kawabata@neoscorp.jp>
2017-02-01 07:57:55 +0900

    format and fixed bugs

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const wpi = require('node-wiring-pi');
 
 const sysfs = require('./lib/readExports.js');
-// const GPIOAccessory = require('./lib/GPIOAccessory.js');
 const JEMAAccessory = require('./lib/JEMAAccessory.js');
 const AutoExport = require('./lib/autoExport.js');
 
 var Accessory, Service, Characteristic, UUIDGen;
 
 
-module.exports = function(homebridge) {
-  
+module.exports = function (homebridge) {
+
   // Accessory must be created from PlatformAccessory Constructor
   Accessory = homebridge.platformAccessory;
 
@@ -27,13 +26,16 @@ function WPiPlatform(log, config, api) {
   var platform = this;
   this.log = log;
   this.config = config;
-  this.gpiopins = this.config.terminals.gpiopins || [];
+  this.terminals = this.config.terminals;
   this.accessories = [];
+  this.gpiopins = [];
+  for (var i in this.terminals) {
+    this.gpiopins.push(this.terminals[i].monitorPin, this.terminals[i].controlPin);
+  }
 
   //Export pins via sysfs if enabled with autoExport
-  if((typeof this.config.autoExport !== undefined) && (this.config.autoExport === "true"))
-  {
-      AutoExport(this.log, this.gpiopins);
+  if ((typeof this.config.autoExport !== undefined) && (this.config.autoExport === "true")) {
+    AutoExport(this.log, this.gpiopins);
   }
 
   //Configure wiring pi using 'sys' mode - requires pins to
@@ -41,31 +43,30 @@ function WPiPlatform(log, config, api) {
   wpi.setup('sys');
 
   if (api) {
-      // Save the API object as plugin needs to register new accessory via this object.
-      this.api = api;
-      platform.log("homebridge API version: " + api.version);
+    // Save the API object as plugin needs to register new accessory via this object.
+    this.api = api;
+    platform.log("homebridge API version: " + api.version);
 
-      // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories
-      // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-      // Or start discover new accessories
-      this.api.on('didFinishLaunching', function() {
-        platform.log("Loading cached GPIO pins complete");
-        // for ( var i in this.gpiopins ) { this.addGPIOPin(this.gpiopins[i]); }
-        for ( var i in this.terminals ) { this.addTerminal(this.terminals[i]); }
-        
-        //Start polling all pins...
-        this.statePolling();
-      }.bind(this));
+    // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories
+    // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
+    // Or start discover new accessories
+    this.api.on('didFinishLaunching', function () {
+      platform.log("Loading cached GPIO pins complete");
+      for (var i in this.terminals) { this.addTerminal(this.terminals[i]); }
+
+      //Start polling all pins...
+      this.statePolling();
+    }.bind(this));
   }
 }
 
 // Function invoked when homebridge tries to restore cached accessory
-WPiPlatform.prototype.configureAccessory = function(accessory) {
+WPiPlatform.prototype.configureAccessory = function (accessory) {
   this.log(accessory.displayName, "Configure GPIO Pin", accessory.UUID);
   var platform = this;
 
-  if(platform.config.overrideCache === "true") {
-    var newContext = platform.terminals.find( p => p.name === accessory.context.name );
+  if (platform.config.overrideCache === "true") {
+    var newContext = platform.terminals.find(p => p.name === accessory.context.name);
     // var newContext = platform.gpiopins.find( p => p.name === accessory.context.name );
     accessory.context = newContext;
   }
@@ -73,46 +74,28 @@ WPiPlatform.prototype.configureAccessory = function(accessory) {
   //Check reachability by querying the sysfs path
   var exportState = sysfs(accessory.context.pin);
 
-  if(!exportState.error) {
-    if(exportState.direction === accessory.context.mode) {
+  if (!exportState.error) {
+    if (exportState.direction === accessory.context.mode) {
       accessory.reachable = true;
     }
   }
 
   accessory.reachable = true;
-  
+
   var onChar;
-  if (accessory.getService(Service.Switch)) { 
+  if (accessory.getService(Service.Switch)) {
     onChar = accessory.getService(Service.Switch).getCharacteristic(Characteristic.On);
   }
-  
-  // var gpioAccessory = new GPIOAccessory(platform.log, accessory, wpi, onChar);
+
   var jemaAccessory = new JEMAAccessory(platform.log, accessory, wpi, onChar);
 
-  // accessory.getService(Service.Switch)
-  //   .getCharacteristic(Characteristic.On)
-  //   .on('get', gpioAccessory.getOn.bind(gpioAccessory))
-  //   .on('set', gpioAccessory.setOn.bind(gpioAccessory));
   accessory.getService(Service.Switch)
     .getCharacteristic(Characteristic.On)
     .on('get', jemaAccessory.getOn.bind(jemaAccessory))
     .on('set', jemaAccessory.setOn.bind(jemaAccessory));
-  
-  // if (accessory.getService(Service.Switch) && accessory.context.mode === "out") {
-  //   accessory.getService(Service.Switch)
-  //     .getCharacteristic(Characteristic.On)
-  //     .on('get', gpioAccessory.getOn.bind(gpioAccessory))
-  //     .on('set', gpioAccessory.setOn.bind(gpioAccessory));
-  // }
-
-  // if (accessory.getService(Service.ContactSensor) && accessory.context.mode === "in") {
-  //   accessory.getService(Service.ContactSensor)
-  //     .getCharacteristic(Characteristic.ContactSensorState)
-  //     .on('get', gpioAccessory.getOn.bind(gpioAccessory));
-  // }
 
   // Handle the 'identify' event
-  accessory.on('identify', function(paired, callback) {
+  accessory.on('identify', function (paired, callback) {
     platform.log(accessory.displayName, "Identify!!!");
     // TODO: run 3000ms on/off?
     callback();
@@ -124,65 +107,25 @@ WPiPlatform.prototype.configureAccessory = function(accessory) {
 
 //Handler will be invoked when user try to config your plugin
 //Callback can be cached and invoke when nessary
-WPiPlatform.prototype.configurationRequestHandler = function(context, request, callback) {
+WPiPlatform.prototype.configurationRequestHandler = function (context, request, callback) {
   console.log("Not Implemented");
 }
 
-// Sample function to show how developer can add accessory dynamically from outside event
-// WPiPlatform.prototype.addGPIOPin = function(gpiopin) {
-//   var platform = this;
-//   var uuid;
-
-//   uuid = UUIDGen.generate(gpiopin.name);
-
-//   var uuidExists = this.accessories.filter(function(item) {
-//     return item.UUID == uuid;
-//   }).length;
-
-//   if (uuidExists == 0) {
-//     this.log("New GPIO from config.json: " + gpiopin.name + " (" + gpiopin.pin + ")");
-  
-//     var newAccessory = new Accessory(gpiopin.name, uuid);
-    
-//     newAccessory.getService(Service.AccessoryInformation)
-//       .setCharacteristic(Characteristic.Manufacturer, platform.config.manufacturer ? platform.config.manufacturer : "Raspberry Pi Foundation")
-//       .setCharacteristic(Characteristic.Model, platform.config.model ? platform.config.model : "Pi GPIO")
-//       .setCharacteristic(Characteristic.SerialNumber, platform.config.serial ? platform.config.serial : "Default-SerialNumber");
-
-//     switch(gpiopin.mode) {
-//       case "out":
-//         newAccessory.addService(Service.Switch, gpiopin.name);
-//         break;
-//       case "in":
-//         newAccessory.addService(Service.ContactSensor, gpiopin.name);
-//         break;
-//       default:
-//         platform.log("WARNING: Unsupported GPIO Pin Mode (%s)", gpiopin.mode);
-//     }
-    
-//     newAccessory.context = gpiopin;
-        
-//     this.configureAccessory(newAccessory);
-//     this.api.registerPlatformAccessories("homebridge-WPiJEMAPlatform", "WPiPlatform", [newAccessory]);
-//   }
-// }
-
-WPiPlatform.prototype.addTerminal = function(terminal) {
+WPiPlatform.prototype.addTerminal = function (terminal) {
   var platform = this;
   var uuid;
 
-  // uuid = UUIDGen.generate(gpiopin.name);
   uuid = UUIDGen.generate(terminal.name);
 
-  var uuidExists = this.accessories.filter(function(item) {
+  var uuidExists = this.accessories.filter(function (item) {
     return item.UUID == uuid;
   }).length;
 
   if (uuidExists == 0) {
-    this.log("New JEMA Terminal from config.json: " + terminal.name + " (" + terminal.pin + ")");
-  
+    this.log("New JEMA Terminal from config.json: " + terminal.name + " (" + terminal.controlPin.pin + ")");
+
     var newAccessory = new Accessory(terminal.name, uuid);
-    
+
     newAccessory.getService(Service.AccessoryInformation)
       .setCharacteristic(Characteristic.Manufacturer, platform.config.manufacturer ? platform.config.manufacturer : "Raspberry Pi Foundation")
       .setCharacteristic(Characteristic.Model, platform.config.model ? platform.config.model : "Pi GPIO")
@@ -190,26 +133,14 @@ WPiPlatform.prototype.addTerminal = function(terminal) {
 
     newAccessory.addService(Service.Switch, terminal.name);
 
-    // switch(gpiopin.mode) {
-    //   case "out":
-    //     newAccessory.addService(Service.Switch, gpiopin.name);
-    //     break;
-    //   case "in":
-    //     newAccessory.addService(Service.ContactSensor, gpiopin.name);
-    //     break;
-    //   default:
-    //     platform.log("WARNING: Unsupported GPIO Pin Mode (%s)", gpiopin.mode);
-    // }
-    
     newAccessory.context = terminal;
-        
+
     this.configureAccessory(newAccessory);
     this.api.registerPlatformAccessories("homebridge-WPiJEMAPlatform", "WPiPlatform", [newAccessory]);
   }
 }
 
-
-WPiPlatform.prototype.updateAccessoriesReachability = function() {
+WPiPlatform.prototype.updateAccessoriesReachability = function () {
   this.log("Update Reachability");
   for (var index in this.accessories) {
     var accessory = this.accessories[index];
@@ -218,7 +149,7 @@ WPiPlatform.prototype.updateAccessoriesReachability = function() {
 }
 
 // Sample function to show how developer can remove accessory dynamically from outside event
-WPiPlatform.prototype.removeAccessory = function(accessory) {
+WPiPlatform.prototype.removeAccessory = function (accessory) {
   this.log("Remove Accessory");
   this.api.unregisterPlatformAccessories("homebridge-WPiJEMAPlatform", "WPiPlatform", this.accessories);
 
@@ -229,30 +160,22 @@ WPiPlatform.prototype.removeAccessory = function(accessory) {
 // Method for state periodic update
 WPiPlatform.prototype.statePolling = function () {
   var platform = this;
-  
+
   // Clear polling
   //clearTimeout(this.tout);
 
   // Setup periodic update with polling interval
   this.tout = setTimeout(function () {
-        // Update states for all HomeKit accessories
-        for (var deviceID in platform.accessories) {
-          var accessory = platform.accessories[deviceID];
-          if(accessory.context.polling === "true") {
-            accessory.getService(Service.Switch).getCharacteristic(Characteristic.On).getValue();
+    // Update states for all HomeKit accessories
+    for (var deviceID in platform.accessories) {
+      var accessory = platform.accessories[deviceID];
+      if (accessory.context.polling === "true") {
+        accessory.getService(Service.Switch).getCharacteristic(Characteristic.On).getValue();
+      }
+    }
 
-            // if (accessory.getService(Service.Switch) && accessory.context.mode === "out") {
-            //   accessory.getService(Service.Switch).getCharacteristic(Characteristic.On).getValue();
-            // }
+    // Setup next polling
+    platform.statePolling();
 
-            // if (accessory.getService(Service.ContactSensor) && accessory.context.mode === "in") {
-            //   accessory.getService(Service.ContactSensor).getCharacteristic(Characteristic.ContactSensorState).getValue();
-            // }
-          }
-        }
-      
-        // Setup next polling
-        platform.statePolling();
-  
   }, 2000);
 }
